f57e90aa69c3eac305618bd7ecb1f357ee041db5
Amrendra <118886890+Amrendra2004@users.noreply.github.com>
2024-05-18 08:28:19 +0530

    Update STRASSEN MATRIX MULTIPICATION.cpp

--- STRASSEN MATRIX MULTIPICATION.cpp
+++ STRASSEN MATRIX MULTIPICATION.cpp
@@ -1,152 +1,48 @@
-#include <iostream>
-#include <vector>
-
+#include<iostream>
 using namespace std;
-
-// Function to add two matrices
-vector<vector<int>> addMatrices(const vector<vector<int>>& A, const vector<vector<int>>& B) {
-    int n = A.size();
-    vector<vector<int>> C(n, vector<int>(n, 0));
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            C[i][j] = A[i][j] + B[i][j];
-        }
-    }
-
-    return C;
-}
-
-// Function to subtract two matrices
-vector<vector<int>> subtractMatrices(const vector<vector<int>>& A, const vector<vector<int>>& B) {
-    int n = A.size();
-    vector<vector<int>> C(n, vector<int>(n, 0));
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            C[i][j] = A[i][j] - B[i][j];
-        }
-    }
-
-    return C;
-}
-
-// Function to multiply two matrices using Strassen's algorithm
-vector<vector<int>> strassen(const vector<vector<int>>& A, const vector<vector<int>>& B) {
-    int n = A.size();
-
-    // Base case: If the matrices are 1x1
-    if (n == 1) {
-        vector<vector<int>> C(1, vector<int>(1, 0));
-        C[0][0] = A[0][0] * B[0][0];
-        return C;
-    }
-
-    // Splitting the matrices into quadrants
-    int newSize = n / 2;
-    vector<vector<int>> A11(newSize, vector<int>(newSize));
-    vector<vector<int>> A12(newSize, vector<int>(newSize));
-    vector<vector<int>> A21(newSize, vector<int>(newSize));
-    vector<vector<int>> A22(newSize, vector<int>(newSize));
-    vector<vector<int>> B11(newSize, vector<int>(newSize));
-    vector<vector<int>> B12(newSize, vector<int>(newSize));
-    vector<vector<int>> B21(newSize, vector<int>(newSize));
-    vector<vector<int>> B22(newSize, vector<int>(newSize));
-
-    // Initializing the quadrants
-    for (int i = 0; i < newSize; i++) {
-        for (int j = 0; j < newSize; j++) {
-            A11[i][j] = A[i][j];
-            A12[i][j] = A[i][j + newSize];
-            A21[i][j] = A[i + newSize][j];
-            A22[i][j] = A[i + newSize][j + newSize];
-            B11[i][j] = B[i][j];
-            B12[i][j] = B[i][j + newSize];
-            B21[i][j] = B[i + newSize][j];
-            B22[i][j] = B[i + newSize][j + newSize];
-        }
-    }
-
-    // Calculating the products
-    vector<vector<int>> P1 = strassen(A11, subtractMatrices(B12, B22));
-    vector<vector<int>> P2 = strassen(addMatrices(A11, A12), B22);
-    vector<vector<int>> P3 = strassen(addMatrices(A21, A22), B11);
-    vector<vector<int>> P4 = strassen(A22, subtractMatrices(B21, B11));
-    vector<vector<int>> P5 = strassen(addMatrices(A11, A22), addMatrices(B11, B22));
-    vector<vector<int>> P6 = strassen(subtractMatrices(A12, A22), addMatrices(B21, B22));
-    vector<vector<int>> P7 = strassen(subtractMatrices(A11, A21), addMatrices(B11, B12));
-
-    // Calculating the quadrants of the result matrix
-    vector<vector<int>> C11 = addMatrices(subtractMatrices(addMatrices(P5, P4), P2), P6);
-    vector<vector<int>> C12 = addMatrices(P1, P2);
-    vector<vector<int>> C21 = addMatrices(P3, P4);
-    vector<vector<int>> C22 = subtractMatrices(subtractMatrices(addMatrices(P5, P1), P3), P7);
-
-    // Combining the quadrants into the result matrix
-    vector<vector<int>> C(n, vector<int>(n));
-    for (int i = 0; i < newSize; i++) {
-        for (int j = 0; j < newSize; j++) {
-            C[i][j] = C11[i][j];
-            C[i][j + newSize] = C12[i][j];
-            C[i + newSize][j] = C21[i][j];
-            C[i + newSize][j + newSize] = C22[i][j];
-        }
-    }
-
-    return C;
-}
-
-// Function to print a matrix
-void printMatrix(const vector<vector<int>>& matrix) {
-    int n = matrix.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-// Function to input a matrix from the user
-vector<vector<int>> inputMatrix(int n) {
-    vector<vector<int>> matrix(n, vector<int>(n));
-    cout << "Enter the elements of the matrix:" << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << "Enter element [" << i << "][" << j << "]: ";
-            cin >> matrix[i][j];
-        }
-    }
-    return matrix;
-}
-
 int main() {
-    int n;
-    cout << "Enter the size of square matrices: ";
-    cin >> n;
+   int z[2][2];
+   int i, j;
+   int m1, m2, m3, m4 , m5, m6, m7;
+      int x[2][2] = {
+         {12, 34}, 
+         {22, 10}
+      };
+   int y[2][2] = {
+      {3, 4}, 
+      {2, 1}
+   };
+   cout<<"The first matrix is: ";
+   for(i = 0; i < 2; i++) {
+      cout<<endl;
+      for(j = 0; j < 2; j++)
+         cout<<x[i][j]<<" ";
+   }
+   cout<<"\nThe second matrix is: ";
+   for(i = 0;i < 2; i++){
+      cout<<endl;
+      for(j = 0;j < 2; j++)
+         cout<<y[i][j]<<" ";
+   }
 
-    // Input matrix A
-    cout << "Enter matrix A:" << endl;
-    vector<vector<int>> A = inputMatrix(n);
+   m1 = (x[0][0] + x[1][1]) * (y[0][0] + y[1][1]);
+   m2 = (x[1][0] + x[1][1]) * y[0][0];
+   m3 = x[0][0] * (y[0][1] - y[1][1]);
+   m4 = x[1][1] * (y[1][0] - y[0][0]);
+   m5 = (x[0][0] + x[0][1]) * y[1][1];
+   m6 = (x[1][0] - x[0][0]) * (y[0][0]+y[0][1]);
+   m7 = (x[0][1] - x[1][1]) * (y[1][0]+y[1][1]);
 
-    // Input matrix B
-    cout << "Enter matrix B:" << endl;
-    vector<vector<int>> B = inputMatrix(n);
+   z[0][0] = m1 + m4- m5 + m7;
+   z[0][1] = m3 + m5;
+   z[1][0] = m2 + m4;
+   z[1][1] = m1 - m2 + m3 + m6;
 
-    // Calculate the result using Strassen's algorithm
-    vector<vector<int>> C = strassen(A, B);
-
-    // Print the result
-    cout << "Matrix A:" << endl;
-    printMatrix(A);
-    cout << endl;
-
-    cout << "Matrix B:" << endl;
-    printMatrix(B);
-    cout << endl;
-
-    cout << "Result of matrix multiplication using Strassen's algorithm:" << endl;
-    printMatrix(C);
-
-    return 0;
+   cout<<"\nProduct achieved using Strassen's algorithm: ";
+   for(i = 0; i < 2 ; i++) {
+      cout<<endl;
+      for(j = 0; j < 2; j++)
+         cout<<z[i][j]<<" ";
+   }
+   return 0;
 }
